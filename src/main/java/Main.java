bdc26b65371f25e3acaade8a89b90ab58832daa2
igor raphael <169068315+igorraphael08@users.noreply.github.com>
2024-07-04 10:55:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(system.in);
+    System.out.println("Informe seu nome: ");
+    String nome = sc.nextLine();
+    System.out.println("Informe seu sobrenome: ");
+    String sobrenome = nextLine();
+System.out.println("informe sua idade: ");
+    int idade = sc.next();
+    
+    sc.close();
+
+  }
+
+}
